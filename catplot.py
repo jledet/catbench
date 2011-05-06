1155aa10ede01303835a51ea68febb6dc2682111
Jeppe Ledet-Pedersen <jeppe@gomspace.com>
2011-05-06 16:14:28 +0200

    Plot stats

--- catplot.py
+++ catplot.py
@@ -37,7 +37,7 @@ c = {
 }
 
 colors_nc = [c["chameleon2"], c["plum2"], c["aluminium5"]]
-colors_no_nc = [c["skyblue3"], c["skyblue1"]]
+colors_no_nc = [c["skyblue3"], c["skyblue1"], c["skyblue2"]]
 colors_cg = [c["scarletred1"], c["scarletred2"], c["orange2"], c["scarletred3"], c["orange3"]]
 
 paper = {
@@ -83,7 +83,6 @@ def set_style(mode):
         pylab.rcParams.update(big_fonts)
     elif mode == 'default':
         pylab.rcParams.setdefault
-        print 'default set'
     else:
         print "warning: no such setting, using default"
         change('default')
@@ -95,8 +94,10 @@ def main():
     set_style('default')
 
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument("--without-nc", required=True, dest="without_nc", default=None)
-    parser.add_argument("--with-nc", required=True, dest="with_nc", default=None)
+    parser.add_argument("--with-nc", required=False, dest="with_nc", default=None)
+    parser.add_argument("--without-nc", required=False, dest="without_nc", default=None)
+    parser.add_argument("--nodes-with-nc", required=False, dest="nodes_with_nc", default=None)
+    parser.add_argument("--nodes-without-nc", required=False, dest="nodes_without_nc", default=None)
     args = parser.parse_args()
 
     for filename in (args.with_nc, args.without_nc):
@@ -112,8 +113,7 @@ def main():
             for line in f:
                 if not line.startswith("#"):
                     slave,test_speed,test,speed,delay,lost,total,pl = line.split(",")
-                    if not slave in meas:
-                        meas[slave] = []
+                    if not slave in meas: meas[slave] = []
                     meas[slave].append((test_speed,test,speed,delay,lost,total,pl))
 
             y = {}
@@ -122,15 +122,15 @@ def main():
                 slave_speeds = {}
                 for m in meas[i]:
                     ts = m[0] # Read test_speed
-                    s =  m[2] # Read speed
+                    s  = m[2] # Read speed
                     if not ts in slave_speeds: slave_speeds[ts] = []
                     slave_speeds[ts].append(float(s))
-                print(slave_speeds)
+                #print(slave_speeds)
                 x[i] = map(lambda l: int(l), slave_speeds)
                 y[i] = map(lambda j: sum(slave_speeds[j])/len(slave_speeds[j]), slave_speeds)
                 x[i],y[i] = zip(*sorted(zip(x[i],y[i]))) # Sort x and y based on x
-                print(x)
-                print(y)
+                #print(x)
+                #print(y)
 
             if filename == args.with_nc:
                 y_nc = y
@@ -139,8 +139,10 @@ def main():
                 y_no_nc = y
                 x_no_nc = x
 
+
     legends = []
     coding_gain = {}
+    pylab.figure(0)
     pylab.hold('on')
     for slave in y_nc:
         legends.append(slave.title() + ", CATWOMAN")
@@ -149,16 +151,95 @@ def main():
         legends.append(slave.title() + ", No CATWOMAN")
         pylab.plot(x_no_nc[slave], y_no_nc[slave], color=colors_no_nc[last_color_no_nc], linewidth=2)
         last_color_no_nc += 1
-        coding_gain[slave] = map(operator.sub, y_nc[slave], y_no_nc[slave])
-        pylab.plot(x_no_nc[slave], coding_gain[slave], color=colors_cg[last_color_cg], linewidth=2)
-        last_color_cg += 1
-        legends.append(slave.title() + " coding gain")
+        if len(y_nc[slave]) == len(y_no_nc[slave]):
+            coding_gain[slave] = map(operator.sub, y_nc[slave], y_no_nc[slave])
+            pylab.plot(x_no_nc[slave], coding_gain[slave], color=colors_cg[last_color_cg], linewidth=2)
+            last_color_cg += 1
+            legends.append(slave.title() + " coding gain")
 
     pylab.plot(x[x.keys()[0]], x[x.keys()[0]], color="#000000", linestyle="--")
-    legends.append("Theoretical limit")
-    pylab.title("Transmit vs. Receive speed")
+    #legends.append("Theoretical limit")
+    pylab.title("Throughput vs. Load")
     pylab.xlabel("Transmit speed [kb/s]")
     pylab.ylabel("Receive speed [kb/s]")
+    pylab.legend(legends, loc='upper left', shadow=True)
+    pylab.grid('on')
+
+    # Plot coding/forward rate
+    legends = []
+    for filename in (args.nodes_with_nc, args.nodes_without_nc):
+        if not filename: continue
+        nmeas = {}
+        try:
+            f = open(filename)
+        except:
+            print("Failed to open {}".format(f))
+            sys.exit(-1)
+        else:
+            # Read in all measurements
+            for line in f:
+                if not line.startswith("#"):
+                    timestamp,node,speed,test,tx,rx,fw,coded,dropped,decoded,failed,dbuf,cbuf,cpu,last = line.split(",")
+                    if not last.strip() == "1": continue
+                    if not node in nmeas: nmeas[node] = []
+                    nmeas[node].append((timestamp,speed,test,tx,rx,fw,coded,dropped,decoded,failed,dbuf,cbuf,cpu))
+
+            y_fw = {}
+            y_coded = {}
+            x = {}
+            for i in nmeas:
+                node_speeds = {}
+                for m in nmeas[i]:
+                    s     = m[1] # Read speed
+                    fw    = m[5] # Read forwared
+                    coded = m[6] # Read coded
+                    if not s in node_speeds: node_speeds[s] = [[],[]]
+                    node_speeds[s][0].append(int(fw))
+                    node_speeds[s][1].append(int(coded))
+                
+                print(node_speeds)
+                x[i] = map(lambda l: int(l), node_speeds)
+                y_fw[i] = map(lambda j: sum(node_speeds[j][0])/len(node_speeds[j][0]), node_speeds)
+                y_coded[i] = map(lambda j: sum(node_speeds[j][1])/len(node_speeds[j][1]), node_speeds)
+                x[i],y_fw[i] = zip(*sorted(zip(x[i],y_fw[i]))) # Sort x and y based on x
+                x[i],y_coded[i] = zip(*sorted(zip(x[i],y_coded[i]))) # Sort x and y based on x
+                #print(x)
+                #print(y_fw)
+                #print(y_coded)
+
+            if filename == args.nodes_with_nc:
+                y_coded_nc = y_coded
+                y_fw_nc = y_fw
+                x_nc = x
+            elif filename == args.nodes_without_nc:
+                y_coded_no_nc = y_coded
+                y_fw_no_nc = y_fw
+                x_no_nc = x
+        finally:
+            f.close()
+
+    last_color_nc = 0
+    last_color_no_nc = 0
+    last_color_cg = 0
+    pylab.figure(1)
+    for node in y_coded_nc:
+        print(node)
+        print(x_nc)
+        print(y_coded_nc)
+        legends.append(node.title() + " coded, CATWOMAN")
+        pylab.plot(x_nc[node], y_coded_nc[node], color=colors_nc[last_color_nc], linewidth=2)
+        last_color_nc += 1
+        legends.append(node.title() + " forwarded, CATWOMAN")
+        pylab.plot(x_nc[node], y_fw_nc[node], color=colors_no_nc[last_color_no_nc], linewidth=2)
+        last_color_no_nc += 1
+        #legends.append(node.title() + " coded, No CATWOMAN")
+        #pylab.plot(x_no_nc[node], y_coded_no_nc[node], color=colors_no_nc[last_color_no_nc], linewidth=2)
+        #legends.append(node.title() + " forwarded, No CATWOMAN")
+        #pylab.plot(x_no_nc[node], y_fw_no_nc[node], color=colors_no_nc[last_color_no_nc], linewidth=2)
+        #last_color_no_nc += 1
+    pylab.title("Coded/Forwarded packets vs. Load")
+    pylab.xlabel("Transmit speed [kb/s]")
+    pylab.ylabel("Packets")
     pylab.legend(legends, loc='upper left', shadow=True)
     pylab.grid('on')
     pylab.show()
